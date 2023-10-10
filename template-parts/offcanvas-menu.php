6ebf979f00eeb2fe4d59f93de8506a14680e8ec7
valentin-trivial <100561068+valentin-trivial@users.noreply.github.com>
2023-10-10 09:22:33 +0200

    Improvements

--- template-parts/offcanvas-menu.php
+++ template-parts/offcanvas-menu.php
@@ -1,4 +1,4 @@
-<div id="offcanvas-menu" class="offcanvas fixed w-80 md:w-96 bg-white shadow-2xl right-0 top-0 h-full z-30 translate-x-80 md:translate-x-96 opacity-0 pointer-events-none transition-all duration-500 ease-out">
+<div id="offcanvas-menu" class="fixed top-0 right-0 z-30 h-full transition-all duration-500 ease-out bg-white shadow-2xl opacity-0 pointer-events-none offcanvas w-80 md:w-96 translate-x-80 md:translate-x-96">
 
    <?php wp_nav_menu(
         array(
@@ -6,7 +6,7 @@
             'container_class' => 'mt-0 relative p-0 bg-transparent block',
             'menu_class'      => 'menu accordion',
             'theme_location'  => 'primary',
-            'li_class'        => 'text-primary  w-64',
+            'li_class'        => 'text-primary',
             'fallback_cb'     => false,
         )
     ); ?>
